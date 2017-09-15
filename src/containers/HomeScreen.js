62a80cbd626c5c407fa6c9b71d2508116fc750d2
Bouchon <knoblauch.florian@gmail.com>
2017-09-15 02:08:30 +0200

    add simple-oauth2

--- src/containers/HomeScreen.js
+++ src/containers/HomeScreen.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
+import autobind from 'autobind-decorator'
 
 import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
 
 import Demo from '../components/test/Demo'
 
 class HomeScreen extends Component {
+
+  @autobind
+  login () {
+    console.log('click...')
+    const req = new XMLHttpRequest()
+    req.withCredentials = true
+    req.open('GET', '/api/v1/auth', true)
+    req.send(null)
+
+    console.log('RESULT', req)
+  }
+
   render () {
     return (
       <div>
+        <Button raised onClick={this.login}>o</Button>
         <Typography type='display4'>Home</Typography>
         <Demo />
       </div>
--- tools/features/api.js
+++ tools/features/api.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import simpleOauthModule from 'simple-oauth2'
 const users = [{
   id: 0,
   logon: 'admin',
@@ -28,6 +28,53 @@ router.post('/sessions', (req, res) => {
       res.status(201).json({ status: 'authenticated' })
     }
   }, 500)
+})
+
+// oauth2
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+const oauth2 = simpleOauthModule.create({
+  client: {
+    id: 'efbb6db1cb8d0b21b46a',
+    secret: '7eab121f4a350d4c8ad774cef19fc3fc34316d6f'
+  },
+  auth: {
+    tokenHost: 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize'
+  }
+})
+const authorizationUri = oauth2.authorizationCode.authorizeURL({
+  redirect_uri: 'http://localhost:3000/api/v1/callback',
+  scope: 'notifications',
+  state: '3(#0!~'
+})
+router.get('/auth', (req, res) => {
+  console.log(authorizationUri)
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.status(200).redirect(authorizationUri)
+})
+router.get('/callback', (req, res) => {
+  console.log('callback!')
+  const code = req.query.code
+  const options = { code }
+  oauth2.authorizationCode.getToken(options, (error, result) => {
+    if (error) {
+      console.error('Access Token Error', error.message)
+      return res.json('Authentication failed')
+    } else {
+      console.log('The resulting token', result)
+      return res.status(200).json(token)
+    }
+  })  
+})
+router.get('/success', (req, res) => {
+  console.log('success')
+  res.send('')
 })
 
 export default router
